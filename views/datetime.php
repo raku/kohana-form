824906acb9562d411a6274601d03eb907c895921
Roman Latyshenko <rl@dalivsoft.com>
2014-02-25 15:21:32 +0200

    some improvements for datetime widget

--- views/datetime.php
+++ views/datetime.php
@@ -1,6 +1,10 @@
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/Kalendae/0.4.1/kalendae.standalone.min.js"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/Kalendae/0.4.1/kalendae.css"/>
+
 <label for="datetime">
     <?php echo ucfirst($name); ?>
 </label>
 
-<input type="text" class="<?php echo implode(" ", $css_classes) ?>" id="datetime" name="<?php echo $name; ?>"
+<input type="text" class="auto-kal <?php echo implode(" ", $css_classes) ?>" id="datetime" name="<?php echo $name; ?>"
        value="<?php echo $value; ?>"/>
