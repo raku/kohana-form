b4306af08f4ef3810f86b75b1d9a82ef39de15ec
NeZanyat <whisper132@mail.ru>
2014-05-23 00:00:51 +0300

    Update ckeditor.php

--- views/nolabels/ckeditor.php
+++ views/nolabels/ckeditor.php
@@ -1,9 +1,5 @@
 <script src="http://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js"></script>
 
-<label for="password">
-    <?php echo ucfirst(str_replace("_", " ", $name)); ?>
-</label>
-
 <textarea name="<?php echo $name ?>" class="<?php echo implode(" ", $css_classes) ?>"
           id="<?php echo $name . $formset_index ?>" cols="30"
           rows="10"><?php echo $value ?></textarea>
