3dfa24fc3aa917eeaf79de88d7a77e837995f4ee
Roman Latyshenko <rl@dalivsoft.com>
2014-06-20 16:24:41 +0300

    "rules" method

--- views/base/checkbox.php
+++ views/base/checkbox.php
@@ -4,7 +4,7 @@
 
 <input style="width:20px;box-shadow: none;"
        onclick="window.document.getElementById('hidden-value').value=Math.abs(window.document.getElementById('hidden-value').value - 1)"
-       class="checkbox" type="checkbox"
+       type="checkbox"
        class="<?php echo implode(" ", $css_classes) ?>"
        id="checkbox"
     <?php echo (bool)$value ? "checked=checked" : ""; ?>/>
